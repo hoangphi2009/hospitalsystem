0bf5ed119f3c806c27af026adad273c9e0cf57f8
Hoang Phi <phido76792@gmail.com>
2023-09-20 12:03:30 +0700

    hi

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,57 @@
+import {useEffect,useRef } from "react";
+import logo from "../../assets/image/logo.png";
+import {NavLink ,Link } from 'react-router-dom';
+
+const navlink = [
+    {
+        path: '/home',
+        display: 'Home'
+    },
+    {
+        path: '/doctors',
+        display: 'Find  a  Doctoc'
+    },
+    {
+        path: '/services',
+        display: 'Services'
+    },
+    {
+        path: '/contact',
+        display: 'Contact'
+    },
+]
+
+const Header = () => {
+  return <header className="header flex items-center">
+    <div className="container">
+        <div className="flex items-center justify-between">
+            {/*==========logo===========*/}
+            <div>
+                <img src={logo} alt="" />
+            </div>
+            
+
+            {/*==========menu===========*/}
+            <div className="navigation">
+                <ul className="menu flex items-center gap-[2.7rem]">
+                    {
+                        navlink.map((link,index)=><li key={index}>
+                            <NavLink to={link.path} className={navClass=>navClass.isActive ? 'text-primaryColor text-[16px] leading-7 font-[600]' : 'text-textColor text-[16px] leading-7 font-[500]'}>
+                                {link.display}
+                            </NavLink>
+                        </li>)
+                    }
+
+                </ul>
+            </div>
+
+
+
+
+
+        </div>
+    </div>
+  </header>
+};
+
+export default Header;
---
+++ frontend/src/layout/Layout/Layout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
+import Routers from '../../routers/Router/Routers'
+
+const Layout = () => {
+  return ( <>
+  <Header />
+  <main>
+    <Routers />
+  </main>
+  <Footer />
+  </>
+  );
+};
+
+export default Layout
